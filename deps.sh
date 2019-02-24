886c7d695775f5c5404864bc984a771ce032cf75
Mariusz Kierski <mariusz.kierski@gmail.com>
2019-02-24 16:05:04 +0100

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt install ruby ruby-sass vim git curl fonts-oxygen fonts-inconsolata gnome-tweak-tool \
-  libglib2.0-dev libgdk-pixbuf2.0-dev libxml2-utils
-cd /tmp
-git clone 
+  libglib2.0-dev libgdk-pixbuf2.0-dev libxml2-utils &&
+cd /tmp &&
+git clone https://github.com/chaser92/Numix-Frost &&
+make &&
+make install &&
+cd -
